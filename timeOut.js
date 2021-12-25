47cf5fb5a387aa6aaf26e1d954f03ee5bada3c39
Sayed <sayedhossainsifat300@gmail.com>
2021-12-25 22:44:03 +0600

    do it again and aginn

---
+++ timeOut.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    console.log("Amara kew mairala") // setTimeout do for one time
+}, 2000);
+
+setInterval(() => {
+    // console.log("Do something") // But setInterval do it again and again
+}, 3000);
